e00563981f65f9c1651da3f55fd27e6aa13ba9fa
Venky Komuravelly <komuravellyvenky@gmail.com>
2021-07-08 19:45:25 -0700

    Initial commit

---
+++ src/main/java/com/venky/spring/dev/UtilityDispatcher/UtilityDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.venky.spring.dev.UtilityDispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityDispatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venky/spring/dev/UtilityDispatcher/UtilityDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venky.spring.dev.UtilityDispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityDispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
